4c84a8013fe2e320cce5ca2cd17b02801bf936de
Lidiacon <lidiacon@ucm.es>
2018-02-19 10:33:27 +0100

    Add files via upload

---
+++ Fase1/src/AnalizadorLexico.java
@@ -0,0 +1,161 @@
+import java.io.FileInputStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class AnalizadorLexico {
+   private Reader input;
+   private StringBuffer lex;
+   private int sigCar;
+   private int filaInicio;
+   private int columnaInicio;
+   private int filaActual;
+   private int columnaActual;
+   private static String NL = System.getProperty("line.separator");
+   
+   private static enum Estado {
+	   INICIO, REC_POR, REC_DIV, REC_PARAB, REC_PARCE, REC_PCOMA, REC_IGUAL,
+	   REC_MAS, REC_MENOS, REC_IDEN, REC_NUMERO, REC_EOF
+    
+   }
+
+   private Estado estado;
+
+   public AnalizadorLexico(Reader input) throws IOException {
+    this.input = input;
+    lex = new StringBuffer();
+    sigCar = input.read();
+    filaActual=1;
+    columnaActual=1;
+   }
+   
+   public UnidadLexica sigToken() throws IOException {
+     estado = Estado.INICIO;
+     filaInicio = filaActual;
+     columnaInicio = columnaActual;
+     lex.delete(0,lex.length());
+     while(true) {
+         switch(estado) {
+           case INICIO:
+        	   if(hayLetra()) transita(Estado.REC_IDEN);
+        	   if(hayPComa())
+         }
+     }    
+   }
+   private void transita(Estado sig) throws IOException {
+     lex.append((char)sigCar);
+     sigCar();         
+     estado = sig;
+   }
+   private void transitaIgnorando(Estado sig) throws IOException {
+     sigCar();         
+     filaInicio = filaActual;
+     columnaInicio = columnaActual;
+     estado = sig;
+   }
+   private void sigCar() throws IOException {
+     sigCar = input.read();
+     if (sigCar == NL.charAt(0)) saltaFinDeLinea();
+     if (sigCar == '\n') {
+        filaActual++;
+        columnaActual=0;
+     }
+     else {
+       columnaActual++;  
+     }
+   }
+   private void saltaFinDeLinea() throws IOException {
+      for (int i=1; i < NL.length(); i++) {
+          sigCar = input.read();
+          if (sigCar != NL.charAt(i)) error();
+      }
+      sigCar = '\n';
+   }
+   
+   private boolean hayLetra() {return sigCar >= 'a' && sigCar <= 'z' ||
+                                      sigCar >= 'A' && sigCar <= 'z';}
+   private boolean hayDigitoPos() {return sigCar >= '1' && sigCar <= '9';}
+   private boolean hayCero() {return sigCar == '0';}
+   private boolean hayDigito() {return hayDigitoPos() || hayCero();}
+   private boolean haySuma() {return sigCar == '+';}
+   private boolean hayResta() {return sigCar == '-';}
+   private boolean hayMul() {return sigCar == '*';}
+   private boolean hayDiv() {return sigCar == '/';}
+   private boolean hayPAp() {return sigCar == '(';}
+   private boolean hayPCierre() {return sigCar == ')';}
+   private boolean hayIgual() {return sigCar == '=';}
+   private boolean hayPComa() {return sigCar == ';';}
+   private boolean hayPunto() {return sigCar == '.';}
+   private boolean haySep() {return sigCar == ' ' || sigCar == '\t' || sigCar=='\n';}
+   private boolean hayNL() {return sigCar == '\r' || sigCar == '\b' || sigCar == '\n';}
+   private boolean hayEOF() {return sigCar == -1;}
+   
+   private UnidadLexica unidadId() {
+     switch(lex.toString()) {
+         case "bool":  
+            return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.BOOL);
+         case "num":    
+            return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NUM);
+         case "true":    
+             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.TRUE);
+         case "false":    
+             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.FALSE);
+         case "and":    
+             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.AND);
+         case "or":    
+             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.OR);
+         case "not":    
+             return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NOT);             
+         default:    
+            return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.IDEN,lex.toString());     
+      }
+   }     
+   private UnidadLexica unidadEnt() {
+     return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.ENT,lex.toString());     
+   }    
+   private UnidadLexica unidadReal() {
+     return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.REAL,lex.toString());     
+   }    
+   private UnidadLexica unidadMas() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAS);     
+   }    
+   private UnidadLexica unidadMenos() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENOS);     
+   }    
+   private UnidadLexica unidadPor() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.POR);     
+   }    
+   private UnidadLexica unidadDiv() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIV);     
+   }    
+   private UnidadLexica unidadPAp() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PAP);     
+   }    
+   private UnidadLexica unidadPCierre() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PCIERRE);     
+   }    
+   private UnidadLexica unidadIgual() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.IGUAL);     
+   }    
+   private UnidadLexica unidadComa() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.COMA);     
+   }    
+   private UnidadLexica unidadEof() {
+     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EOF);     
+   }    
+   private void error() {
+     System.err.println("("+filaActual+','+columnaActual+"):Caracter inexperado");  
+     System.exit(1);
+   }
+
+   public static void main(String arg[]) throws IOException {
+     Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+     AnalizadorLexico al = new AnalizadorLexico(input);
+     UnidadLexica unidad;
+     do {
+       unidad = al.sigToken();
+       System.out.println(unidad);
+     }
+     while (unidad.clase() != ClaseLexica.EOF);
+    } 
+}
