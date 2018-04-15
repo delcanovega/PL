9aee31fa2b69e3ca3c1426609c7bc734308d0358
delcanovega <delcanovega@gmail.com>
2018-04-15 22:54:58 +0200

    Indentación y formato

--- Fase2/Implementación/PLSintax/src/JLex/UnidadLexicaUnivaluada.java
+++ Fase2/Implementación/PLSintax/src/JLex/UnidadLexicaUnivaluada.java
@@ -1,11 +1,16 @@
 package JLex;
 
 public class UnidadLexicaUnivaluada extends UnidadLexica {
-   public String lexema() {throw new UnsupportedOperationException();}   
-   public UnidadLexicaUnivaluada(int fila, ClaseLexica clase) {
-     super(fila,clase);  
-   }
-  public String toString() {
-    return "[clase:"+clase()+",fila:"+fila()+",col:"+columna()+"]";  
-  }   
+	
+	public String lexema() {
+		throw new UnsupportedOperationException();
+	}
+
+	public UnidadLexicaUnivaluada(int fila, ClaseLexica clase) {
+		super(fila, clase);
+	}
+
+	public String toString() {
+		return "[clase:" + clase() + ",fila:" + fila() + ",col:" + columna() + "]";
+	}
 }
