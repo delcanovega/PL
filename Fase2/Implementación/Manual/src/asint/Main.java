c9f652b89f5270f9907d8eb90ede67af9c97bfb3
delcanovega <delcanovega@gmail.com>
2018-06-01 19:43:15 +0200

    Fase 2: Main.java duplicado borrado

--- Fase2/Implementación/Manual/src/asint/Main.java
+++ Fase2/Implementación/Manual/src/asint/Main.java
@@ -7,11 +7,10 @@ import java.io.Reader;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+			Reader input = new InputStreamReader(new FileInputStream("input2.txt"));
 			AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(input);
 			asint.Sp();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
